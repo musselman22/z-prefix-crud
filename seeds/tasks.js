b776e2446b4d1714c19f81af03bf3ac5ff6d4c67
Jacob Musselman <musselmj@oregonstate.edu>
2021-07-19 14:47:40 -0700

    add routes

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -1,8 +1,8 @@
 
 exports.seed = function (knex) {
-  return knex('blogs').del()
+  return knex('tasks').del()
     .then(function () {
-      return knex('blogs').insert([
+      return knex('tasks').insert([
         { title: 'Mow Lawn', description: 'Mow the front and back lawn.  Use the string trimmer after', status: 'Complete' },
         { title: 'Dishes', description: 'Stack plates in dishwasher and hand wash the pans', status: 'Partially complete' },
         { title: 'Cook Dinner', description: 'Get the frozen fish sticks out of freezer and place in over for 30 minutes at 350 degrees', status: 'Need to do' }
