7d28527c912a6f2880aa1ca96c9d10e5f1f176e3
Jacob Musselman <musselmj@oregonstate.edu>
2021-07-19 14:25:11 -0700

    add client

--- client/src/Components/Tasks.js
+++ client/src/Components/Tasks.js
@@ -4,7 +4,7 @@ function Tasks() {
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/tasks')
+    fetch('/api/tasks')
       .then(res => res.json())
       .then(data => {
         console.log(data)
